25368524d2763068569ac7be520559c485f957bc
Matan Borenkraout <matanbobi@gmail.com>
2020-03-25 09:47:05 +0200

    Add missing key

--- src/components/PokemonColor/index.jsx
+++ src/components/PokemonColor/index.jsx
@@ -5,7 +5,7 @@ const PokemonColor = ({ types }) => (
   <div className="pokemon-color">
     <h5>Types:</h5>
     <ul className="types-list">
-      {types && types.map(({ type }) => <TypeItem type={type} />)}
+      {types && types.map(({ type }) => <TypeItem key={type.name} type={type} />)}
     </ul>
   </div>
 )
